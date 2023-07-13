431fa22baa8dc9fc1069a0aae62e62ec0c952433
“Web <Web@digiwagon.com>
2023-07-13 18:18:15 -0400

    added database related config

--- fairhire/settings.py
+++ fairhire/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+import dj_database_url
 STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
@@ -145,3 +145,8 @@ EMAIL_HOST_PASSWORD = 'mafoarbccfyrjbxc'
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default']['OPTIONS']['charset'] = 'utf8mb4'
+del DATABASES['default']['OPTIONS']['sslmode'] 
+DATABASES['default']['OPTIONS']['ssl'] =  {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}
